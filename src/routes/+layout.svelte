72df3fc2df5f6d7edf13472f7079e99f60cde6c6
JeevanLenka <jeevanlenka@JeevanLenkas-MacBook-Air.local>
2024-09-03 04:05:18 +0530

    Complete project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import "../app.css";
+    import "../components/Footer.svelte"
+  </script>
+  
+  <slot />
+  <!-- <Footer/> -->
+  
+
+
+  <!-- List of sections -->
+<!-- header
+hero
+product description
+reviews
+FAQs
+conversion
+footer  -->
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Product from './../components/Product.svelte';
+  import Header from '../components/Header.svelte';
+    import Hero from './../components/Hero.svelte';
+  import Reviews from '../components/Reviews.svelte';
+  import CtAs from '../components/CTAs.svelte';
+  import FaQs from '../components/FAQs.svelte';
+  import Footer from '../components/Footer.svelte';
+  import Conversion from '../components/Conversion.svelte';
+</script>
+
+<main class="flex flex-col">
+    <Header/>
+    <Hero/>
+    <Product/>
+    <Reviews/>
+    <FaQs/>
+    <!-- <CtAs/> -->
+    <Conversion/>
+    <Footer/>
+</main>
